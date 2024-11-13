5276df325b88a82c15561b7ea4568a5cdc928a6b
egroepper <ellen.groepper@student.htw-berlin.de>
2024-11-13 20:56:51 +0100

    ToDos für Methoden und Klassenimplmementierung erstellt

--- Assets/Scripts/Managers/Spawner.cs
+++ Assets/Scripts/Managers/Spawner.cs
@@ -1,9 +1,15 @@
 using UnityEngine;
-
-
 namespace Managers
+
 {
     public class Spawner : MonoBehaviour
     {
+        //toDo: Behaviour anpassen/recherchieren, evt. SimulationBehaviour. Was brauchen wir hier?
+        //toDo: welche Felder braucht es hier? 
     }
+    
+     // private void SpawnPlayer()
+     
+     // private Vector3 GetSpawnPosition()
+     
 }
