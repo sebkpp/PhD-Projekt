a6dec90cfb8ea14377cd86ac5da44ebbb717e0e2
Paula Madita Ingrid Rachel Schnatsmeyer <paula.schnatsmeyer@student.htw-berlin.de>
2024-11-20 16:10:34 +0000

    Spawner und Connection Manager zusammen gelegt, Joined und Left-Methoden vorhanden

--- Assets/Scripts/Managers/Spawner.cs
+++ Assets/Scripts/Managers/Spawner.cs
@@ -2,14 +2,40 @@ using UnityEngine;
 namespace Managers
 
 {
-    public class Spawner : MonoBehaviour
+    public class Spawner : MonoBehaviour, IPlayerJoined, IPlayerLeft
+
+    /*[SerializeField] private unserPlayerPrefab playerPrefab;
+    [Networked, Capacity(2)] private NetworkDic<PlayerRef, Player> Players => default;
     {
         //toDo: Behaviour anpassen/recherchieren, evt. SimulationBehaviour. Was brauchen wir hier?
         //toDo: welche Felder braucht es hier? 
     }
+
     
-     // private void SpawnPlayer()
+    nur Host soll Spwanen erlauben
+     private void PlayerJoined(PlayerRef player)
+     {
+        if (HasStateAuthority)
+        {
+            NetworkObject playerObject = Runner.Spawn(unserPlayerPrefab, Vector3.up, Quaternion.identity, player)
+            Players.Add(player, playerObject.GetComponent<Player>());
+        }
+     }
+
+
+     private void PlayerLeft(PlayerRef player)
+     {
+        if (!HasStateAuthority)
+            return;
+
+        if(Players.TryGet(plyer, out Player playerBehaviour))
+        {
+            Players.remove(player);
+            Runner.Despawn(playerBehaviour.Object);
+        }
+     }
+
      
-     // private Vector3 GetSpawnPosition()
-     
+     private Vector3 GetSpawnPosition()
+     */
 }
