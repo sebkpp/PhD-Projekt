8faed6064463a0cd8c5ebea359213cc7805d27c0
SebKpp <Sebastian.Keppler@htw-berlin.de>
2025-03-20 13:22:05 +0100

    Added a XRInputProvider/Consumer Pattern to pool Input for networking

---
+++ Assets/Application/Scripts/Network/Input/XRInputState.cs
@@ -0,0 +1,27 @@
+﻿using Fusion;
+using UnityEngine;
+
+namespace Application.Scripts.Network.Input
+{
+    /// <summary>
+    /// Saves an Input-State. contains Position/Rotation of PlayerArea, Head, Left and Right Hand
+    /// </summary>
+    public struct XRInputState : INetworkInput
+    {
+        // Player Area, e.g., for teleportation
+        public Vector3 PlayAreaPosition { get; set; }
+        public Quaternion PlayAreaRotation { get; set; }
+
+        // Head
+        public Vector3 HeadsetPosition { get; set; }
+        public Quaternion HeadsetRotation { get; set; }
+
+        // Left Hand
+        public Vector3 LeftHandPosition { get; set; }
+        public Quaternion LeftHandRotation { get; set; }
+
+        // Right Hand
+        public Vector3 RightHandPosition { get; set; }
+        public Quaternion RightHandRotation { get; set; }
+    }
+}
