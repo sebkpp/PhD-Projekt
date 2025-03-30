71d1cb5cc47dfdc95d47f992aa3766ee85855e36
SebKpp <Sebastian.Keppler@htw-berlin.de>
2025-03-30 22:53:27 +0200

    Added Tracking-States

---
+++ Assets/Application/Scripts/Interaction/States/HandState.cs
@@ -0,0 +1,23 @@
+﻿using Application.Scripts.Interaction;
+using UnityEngine;
+
+namespace Application.Scripts.Network.Input
+{
+    public struct FingerState
+    {
+        public TransformState Proximal { get; set; }
+        public TransformState Intermediate { get; set; }
+        public TransformState Distal { get; set; }
+    }
+    
+    public struct HandState
+    {
+        public TransformState Wrist { get; set; }
+        
+        public FingerState Thumb { get; set; }
+        public FingerState Index { get; set; }
+        public FingerState Middle { get; set; }
+        public FingerState Ring { get; set; }
+        public FingerState Pinky { get; set; }
+    }
+}
