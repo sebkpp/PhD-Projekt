0941ae4c15a82698dabebe95970ff6c872a430e9
SebKpp <Sebastian.Keppler@htw-berlin.de>
2025-03-30 22:50:34 +0200

    Added Class Extension Helpers

---
+++ Assets/Application/Scripts/Utils/Extensions/TransformExtension.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Application.Scripts.Utils.Extensions
+{
+    public static class TransformExtension
+    {
+        public static Vector3 ApplyOffset(this Transform t, Vector3 positionOffset)
+        {
+            return t.TransformPoint(positionOffset);
+        }
+
+        public static Quaternion ApplyOffsetRotation(this Transform t, Vector3 rotationOffset)
+        {
+            return t.rotation * Quaternion.Euler(rotationOffset);
+        }
+        
+        public static Quaternion ApplyOffsetLocalRotation(this Transform t, Vector3 rotationOffset)
+        {
+            return t.localRotation * Quaternion.Euler(rotationOffset);
+        }
+    }
+}
