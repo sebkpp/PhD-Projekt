06ca942ed1f0fff96eeb1287ea2f8d365fb696f9
SebKpp <Sebastian.Keppler@htw-berlin.de>
2025-03-30 22:55:06 +0200

    Added first networked grabbing and some missing meta files

---
+++ Assets/Application/Scripts/Interaction/Grabber.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections.Generic;
+using Application.Scripts.Avatar;
+using Application.Scripts.InteractableObject;
+using Application.Scripts.Network.Interaction;
+using UnityEngine;
+
+namespace Application.Scripts.Interaction
+{
+    public class Grabber : MonoBehaviour
+    {
+        HardwareHand hand;
+
+        Collider lastCheckedCollider;
+        Grabbable lastCheckColliderGrabbable;
+        public Grabbable grabbedObject;
+        // Will be set by the NetworkGrabber for the local user itself, when it spawns
+        public NetworkedGrabber networkGrabber;
+        protected virtual void Awake()
+        {
+            //hand = GetComponentInParent<HardwareHand>();
+            //if (hand == null) Debug.LogError("Grabber should be placed next to an hardware hand");
+        }
+
+        protected virtual bool IsGrabbing => hand && hand.isGrabbing;
+
+        private void OnTriggerStay(Collider other)
+        {
+            if (enabled == false) return;
+            // Exit if an object is already grabbed
+            if (grabbedObject != null)
+            {
+                // It is already the grabbed object or another, but we don't allow shared grabbing here
+                return;
+            }
+
+            Grabbable grabbable;
+
+            if (lastCheckedCollider == other)
+            {
+                grabbable = lastCheckColliderGrabbable;
+            }
+            else
+            {
+                grabbable = other.GetComponentInParent<Grabbable>();
+            }
+            // To limit the number of GetComponent calls, we cache the latest checked collider grabbable result
+            lastCheckedCollider = other;
+            lastCheckColliderGrabbable = grabbable;
+            if (grabbable != null)
+            {
+                if (IsGrabbing)
+                {
+                    Grab(grabbable);
+                } 
+            }
+        }
+        
+        public virtual void Grab(Grabbable grabbable)
+        {
+            grabbable.Grab(this);
+            grabbedObject = grabbable;
+        }
+
+        public virtual void Ungrab(Grabbable grabbable)
+        {
+            if (grabbedObject == null) return;
+            grabbedObject.Ungrab();
+            grabbedObject = null;
+        }
+
+        private void Update()
+        {
+            // Check if the local hand is still grabbing the object
+            if (grabbedObject != null && IsGrabbing == false)
+            {
+                // Object released by this hand
+                Ungrab(grabbedObject);
+            }
+        }
+    }
+}
---
+++ Assets/Application/Scripts/Network/Interaction/NetworkedGrabber.cs
@@ -0,0 +1,28 @@
+﻿using Application.Scripts.Interaction;
+using Application.Scripts.Network.Input;
+using Fusion;
+using UnityEngine;
+
+namespace Application.Scripts.Network.Interaction
+{
+    public class NetworkedGrabber : NetworkBehaviour
+    {
+        [HideInInspector]
+        public NetworkHand hand;
+        
+        public override void Spawned()
+        {
+            base.Spawned();
+            hand = GetComponentInParent<NetworkHand>();
+            if (hand && hand.IsLocalNetworkRig)
+            {
+                // References itself in its local counterpart, to simplify the lookup during local grabbing
+                if (hand.LocalHardwareHand)
+                {
+                    Grabber grabber = hand.LocalHardwareHand.GetComponentInChildren<Grabber>();
+                    grabber.networkGrabber = this;
+                }
+            }
+        }
+    }
+}
