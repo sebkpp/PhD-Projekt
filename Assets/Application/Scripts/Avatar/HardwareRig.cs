87eec37f6d3af3f96a7148c456e1493f65cec8e4
SebKpp <Sebastian.Keppler@htw-berlin.de>
2025-04-08 18:04:01 +0200

    Updated handtracking via networked properties

--- Assets/Application/Scripts/Avatar/HardwareRig.cs
+++ Assets/Application/Scripts/Avatar/HardwareRig.cs
@@ -1,6 +1,5 @@
-using Application.Scripts.Avatar.Utils;
 using Application.Scripts.Interaction;
-using Application.Scripts.Network.Input;
+using Application.Scripts.Interaction.States;
 using Application.Scripts.Utils.Extensions;
 using UnityEngine;
 
@@ -31,16 +30,8 @@ namespace Application.Scripts.Avatar
                 };
                 
                 _rigState.Head = headset.transform.GetTransformState();
-
-                // HandState hand = _rigState.LeftHand;
-                // hand.Wrist = leftHand.HandTrackingData.GetWristTransform(_avatarConfig.LeftHand.wrist);
-                // hand = leftHand.HandTrackingData.GetHandPose(_avatarConfig.LeftHand);
-                _rigState.LeftHandWrist = leftHand.transform.GetTransformState();
+                
                 _rigState.LeftHand = leftHand.HandTrackingData.GetHandPose();
-
-                // HandState state = _rigState.RightHand;
-                // state.Wrist = rightHand.HandTrackingData.GetWristTransform(_avatarConfig.RightHand.wrist);
-                _rigState.RightHandWrist = rightHand.transform.GetTransformState();
                 _rigState.RightHand = rightHand.HandTrackingData.GetHandPose();;
                 
                 //_rigState.leftHandCommand = leftHand.handCommand;
