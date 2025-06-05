908fb2a3959851bdc4192000046b651b1c27d8f3
SebKpp <Sebastian.Keppler@htw-berlin.de>
2025-06-05 15:34:27 +0200

    Test of NetworkedRigidBody

--- Assets/Application/Scripts/Interaction/Grabber.cs
+++ Assets/Application/Scripts/Interaction/Grabber.cs
@@ -1,81 +1,109 @@
-﻿using System.Collections.Generic;
-using Application.Scripts.Avatar;
+﻿using Application.Scripts.Avatar;
 using Application.Scripts.InteractableObject;
 using Application.Scripts.Network.Interaction;
+using Application.Scripts.Utils.Extensions;
 using UnityEngine;
+using VirtualGrasp;
 
 namespace Application.Scripts.Interaction
 {
     public class Grabber : MonoBehaviour
     {
-        HardwareHand hand;
+        private HardwareHand _hand;
 
-        Collider lastCheckedCollider;
-        Grabbable lastCheckColliderGrabbable;
+        private Collider _lastCheckedCollider;
+        private Grabbable _lastCheckColliderGrabbable;
         public Grabbable grabbedObject;
+        
         // Will be set by the NetworkGrabber for the local user itself, when it spawns
         public NetworkedGrabber networkGrabber;
+        
+        protected virtual bool IsGrabbing => _hand && _hand.isGrabbing;
+
+        
+        
         protected virtual void Awake()
         {
-            //hand = GetComponentInParent<HardwareHand>();
-            //if (hand == null) Debug.LogError("Grabber should be placed next to an hardware hand");
-        }
-
-        protected virtual bool IsGrabbing => hand && hand.isGrabbing;
-
-        private void OnTriggerStay(Collider other)
-        {
-            if (enabled == false) return;
-            // Exit if an object is already grabbed
-            if (grabbedObject != null)
-            {
-                // It is already the grabbed object or another, but we don't allow shared grabbing here
-                return;
-            }
-
-            Grabbable grabbable;
-
-            if (lastCheckedCollider == other)
-            {
-                grabbable = lastCheckColliderGrabbable;
-            }
-            else
-            {
-                grabbable = other.GetComponentInParent<Grabbable>();
-            }
-            // To limit the number of GetComponent calls, we cache the latest checked collider grabbable result
-            lastCheckedCollider = other;
-            lastCheckColliderGrabbable = grabbable;
-            if (grabbable != null)
-            {
-                if (IsGrabbing)
-                {
-                    Grab(grabbable);
-                } 
-            }
+            _hand = GetComponentInParent<HardwareHand>();
+            if (_hand == null) Debug.LogError("Grabber should be placed next to an hardware hand");
         }
         
+        private void OnEnable()
+        {
+            Debug.Log($"<color=#ADD8E6>[Grasp]</color> Register Grasp Events");
+            VG_Controller.OnObjectGrasped.AddListener(GraspObject);
+            VG_Controller.OnObjectReleased.AddListener(ReleaseObject);
+        }
+
+        private void OnDisable()
+        {
+            Debug.Log($"<color=#ADD8E6>[Grasp]</color> UnregisterGrasp Events");
+            VG_Controller.OnObjectGrasped.RemoveListener(GraspObject);
+            VG_Controller.OnObjectReleased.RemoveListener(ReleaseObject);
+        }
+
         public virtual void Grab(Grabbable grabbable)
         {
             grabbable.Grab(this);
             grabbedObject = grabbable;
+            _hand.isGrabbing = true;
         }
-
+        
         public virtual void Ungrab(Grabbable grabbable)
         {
             if (grabbedObject == null) return;
             grabbedObject.Ungrab();
             grabbedObject = null;
+            _hand.isGrabbing = false;
+        }
+        
+        private void GraspObject(VG_HandStatus graspInfo)
+        {
+            // Check if the ones who is sending the event has the same handedness as us
+            if (graspInfo.m_side.ToRigPart() != _hand.side) return;
+            
+            Grabbable graspable = graspInfo.m_selectedObject.gameObject.GetComponentInParent<Grabbable>();
+            
+            Debug.Log($"<color=#ADD8E6>[Grasp:Local]</color>Try to grab object {graspable.gameObject.name} with {gameObject.name}");
+            
+            Grab(graspable);
+
+            // if (graspable.Grab(this))
+            // {
+            //     //Debug.Log($"<color=#ADD8E6>[Grasp:Local]</color> {graspable.gameObject.name} grasped up by {gameObject.name}");
+            //     //graspedObject = graspable;
+            // }
+        }
+        
+        private void ReleaseObject(VG_HandStatus graspInfo)
+        {
+            // Check if the ones who is sending the event has the same handedness as us
+            if (graspInfo.m_side.ToRigPart() != _hand.side) return;
+
+            Grabbable releasedObject = graspInfo.m_selectedObject.gameObject.GetComponentInParent<Grabbable>();
+            
+            Debug.Log($"<color=#ADD8E6>[Grasp:Local]</color>Try to release object {releasedObject.gameObject.name} with {gameObject.name}");
+            if (releasedObject.networkGrabbable)
+            {
+                // Transform networkedObjectTransform = releasedObject.networkGrabbable.transform;
+                //
+                // _unGrabPosition = networkedObjectTransform.position;
+                // _unGrabRotation = networkedObjectTransform.rotation;
+                // _unGrabVelocity = releasedObject.Velocity;
+                // _unGrabAngularVelocity = releasedObject.AngularVelocity;
+            }
+
+            Ungrab(grabbedObject);
         }
 
-        private void Update()
-        {
-            // Check if the local hand is still grabbing the object
-            if (grabbedObject != null && IsGrabbing == false)
-            {
-                // Object released by this hand
-                Ungrab(grabbedObject);
-            }
-        }
+        // private void Update()
+        // {
+        //     // Check if the local hand is still grabbing the object
+        //     if (grabbedObject != null && IsGrabbing == false)
+        //     {
+        //         // Object released by this hand
+        //         Ungrab(grabbedObject);
+        //     }
+        // }
     }
 }
