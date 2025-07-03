0a729b0668cabc896462c965320930f2fb5e52bf
s0570833 <s0570833@htw-berlin.de>
2025-07-03 10:33:31 +0200

    all_lamps_on

--- Assets/Plugins/x86_64/TouchTriggerBLE.cs
+++ Assets/Plugins/x86_64/TouchTriggerBLE.cs
@@ -30,21 +30,32 @@ public class TouchTriggerBLE : MonoBehaviour
     {
         switch (originalName)
         {
+            // Rechte Hand
             case "ThumbC_R_end": return "Finger_5";
             case "IndexC_R_end": return "Finger_6";
             case "MiddleC_R_end": return "Finger_7";
             case "RingC_R_end": return "Finger_8";
             case "PinkyC_R_end": return "Finger_9";
+
+            // Linke Hand
+            case "ThumbC_L_end": return "Finger_0";
+            case "IndexC_L_end": return "Finger_1";
+            case "MiddleC_L_end": return "Finger_2";
+            case "RingC_L_end": return "Finger_3";
+            case "PinkyC_L_end": return "Finger_4";
+
             default: return originalName;
         }
-    }
-
+        }
     string ExtractFingerID(string name)
     {
         // Extrahiert die Zahl hinter dem letzten Unterstrich
         int underscoreIndex = name.LastIndexOf('_');
         return (underscoreIndex >= 0 && underscoreIndex < name.Length - 1)
-            ? name.Substring(underscoreIndex + 1)
-            : "X";
+                ? name.Substring(underscoreIndex + 1)
+                : "X";
     }
+
+
 }
+
