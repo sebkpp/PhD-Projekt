6643baf100fc0cbee4593dc6f705de94945d92e0
s0570833 <s0570833@htw-berlin.de>
2025-07-14 13:16:20 +0200

    vibration rechte hand klappt

--- Assets/Plugins/x86_64/TouchTriggerBLE.cs
+++ Assets/Plugins/x86_64/TouchTriggerBLE.cs
@@ -8,10 +8,7 @@ public class TouchTriggerBLE : MonoBehaviour
     {
         if (other.CompareTag("tangible"))
         {
-            string mappedName = MapFingerName(gameObject.name);
-            string msg = "Finger" + ExtractFingerID(mappedName) + "on";
-            bleDevice?.Send(msg);
-            Debug.Log($"[??] Berührung erkannt ? {msg}");
+            SendIntensity(true); // Motor ein
         }
     }
 
@@ -19,11 +16,18 @@ public class TouchTriggerBLE : MonoBehaviour
     {
         if (other.CompareTag("tangible"))
         {
-            string mappedName = MapFingerName(gameObject.name);
-            string msg = "Finger" + ExtractFingerID(mappedName) + "off";
-            bleDevice?.Send(msg);
-            Debug.Log($"[?] Berührung gelöst ? {msg}");
+            SendIntensity(false); // Motor aus
         }
+    }
+
+    void SendIntensity(bool entering)
+    {
+        string mappedName = MapFingerName(gameObject.name);
+        int fingerID = int.Parse(ExtractFingerID(mappedName));
+        int intensity = entering ? 255 : 0;
+        string msg = $"Finger{fingerID}:{intensity}";
+        bleDevice?.Send(msg);
+        Debug.Log($"[TouchTriggerBLE] Finger {fingerID} â†’ IntensitÃ¤t: {intensity}");
     }
 
     string MapFingerName(string originalName)
@@ -31,31 +35,28 @@ public class TouchTriggerBLE : MonoBehaviour
         switch (originalName)
         {
             // Rechte Hand
-            case "Tracker_ThumbC_R_end": return "Finger_5";
-            case "Tracker_IndexC_R_end": return "Finger_6";
+            case "Tracker_ThumbC_R_end":  return "Finger_5";
+            case "Tracker_IndexC_R_end":  return "Finger_6";
             case "Tracker_MiddleC_R_end": return "Finger_7";
-            case "Tracker_RingC_R_end": return "Finger_8";
-            case "Tracker_PinkyC_R_end": return "Finger_9";
+            case "Tracker_RingC_R_end":   return "Finger_8";
+            case "Tracker_PinkyC_R_end":  return "Finger_9";
 
             // Linke Hand
-            case "Tracker_ThumbC_L_end": return "Finger_0";
-            case "Tracker_IndexC_L_end": return "Finger_1";
+            case "Tracker_ThumbC_L_end":  return "Finger_0";
+            case "Tracker_IndexC_L_end":  return "Finger_1";
             case "Tracker_MiddleC_L_end": return "Finger_2";
-            case "Tracker_RingC_L_end": return "Finger_3";
-            case "Tracker_PinkyC_L_end": return "Finger_4";
+            case "Tracker_RingC_L_end":   return "Finger_3";
+            case "Tracker_PinkyC_L_end":  return "Finger_4";
 
             default: return originalName;
         }
     }
+
     string ExtractFingerID(string name)
     {
-        // Extrahiert die Zahl hinter dem letzten Unterstrich
         int underscoreIndex = name.LastIndexOf('_');
         return (underscoreIndex >= 0 && underscoreIndex < name.Length - 1)
-                ? name.Substring(underscoreIndex + 1)
-                : "X";
+            ? name.Substring(underscoreIndex + 1)
+            : "X";
     }
-
-
 }
-
