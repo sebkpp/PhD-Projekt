50a32b710da9f6f8fc8eb08a77c3a658a0264428
SebKpp <Sebastian.Keppler@htw-berlin.de>
2026-03-28 13:28:51 +0100

    feat: add Application.Scripts asmdef, editor asmdef, and test asmdef for EditMode tests

--- Unity/Assets/App/Scripts/ScriptableObjects/AvatarScriptableObject.cs
+++ Unity/Assets/App/Scripts/ScriptableObjects/AvatarScriptableObject.cs
@@ -1,8 +1,10 @@
 using Application.Scripts.Avatar;
 using AYellowpaper.SerializedCollections;
 using Application.Scripts.Utils.Extensions;
-using UnityEditor;
 using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 namespace Application.Scripts.ScriptableObjects
 {
